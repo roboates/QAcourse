8323a2fd5fc2ba252e72dc47967f0fb25b85c6bf
roboates <rob.oates@ukri.org>
2025-01-28 15:46:14 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "RG1"
+    resource_group_name  = "RG3"
     storage_account_name = "jenkinsstaterob"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
